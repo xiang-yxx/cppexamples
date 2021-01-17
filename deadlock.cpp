925c65e872c569b9f8be8a8fab4327af5cc9b94a
xiang <xiang9311@gmail.com>
2021-01-17 10:41:07 +0800

    dead lock example

---
+++ deadlock.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <unistd.h>
+
+
+using namespace std;
+
+class Manager;
+
+class Resource {
+public:
+    void doSomething(const Manager *manager) const __attribute_noinline__;
+    void print() const;
+private:
+    mutable mutex mutex_;
+};
+
+class Manager {
+public:
+    Manager(Resource &resource) : resource_(resource) {}
+    void doSomething() const {
+        lock_guard<mutex> lck(mutex_);
+
+        cout << "manager do some thing, lock manager mutex" << endl;
+        usleep(1000);
+
+        // do some thing
+        resource_.print();
+    }
+private:
+    Resource &resource_;
+    mutable mutex mutex_;
+};
+
+void Resource::doSomething(const Manager *manager) const {
+    lock_guard<mutex> lck(mutex_);
+    
+    cout << "resource do some thing, lock resource mutex" << endl;
+
+    usleep(1000);
+
+    manager->doSomething();
+}
+
+void Resource::print() const {
+    lock_guard<mutex> lck(mutex_);
+    
+    cout << "resource print success" << endl;
+}
+
+
+
+int main() {
+    cout << "dead lock example" << endl;
+
+    Resource resource;
+    Manager manager(resource);
+    thread m(&Manager::doSomething, &manager);
+    // thread r(&Resource::doSomething, &resource, &manager);
+    resource.doSomething(&manager);
+
+    m.join();
+    // r.join();
+
+    cout << "example failed" << endl;
+
+    return 0;
+}
